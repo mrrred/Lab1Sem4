38f836bea0c7758da47a989ffb61f623e796bf9e
mrred <zarubinr85@gmail.com>
2026-02-18 17:45:58 +0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp21
+{
+    public class PS_File
+    {
+
+    }
+
+    public class Product_Signature
+    {
+        private short _length;
+        private int _first;
+        private int _unclaimed;
+        private char[] _spec_name = new char[16];
+        private string _title;
+    }
+
+    public class Product
+    {
+        private byte _del_bit;
+        private int _next_detail_product;
+        private string _content;
+        private int _offset = -1;
+    }
+
+
+
+
+
+
+
+    class Program
+    {
+        static string path = "path.txt";
+        static void Main(string[] args)
+        {
+            if (File.Exists(path))
+            {
+
+            }
+        }
+    }
+}
